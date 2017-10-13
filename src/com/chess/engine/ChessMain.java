0ae3774b16720ac82e1d5f10bcb6598e3b742c5d
DESKTOP-2N1HJOE\otaku <singularity717@gmail.com>
2017-10-13 15:55:54 +0200

    Deleted the JChess and ChessEngine. Modified the ChessMain class

--- src/com/chess/engine/ChessMain.java
+++ src/com/chess/engine/ChessMain.java
@@ -1,9 +1,12 @@
 package com.chess.engine;
 
+import com.chess.engine.board.Board;
 import com.chess.gui.Table;
 
 public class ChessMain {
     public static void main(String[] args){
+        Board board = Board.createStandardBoard();
+        System.out.println(board);
         new Table();
 
     }
