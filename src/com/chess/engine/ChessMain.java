48d7c058d4792ad6176da8337848106828ecb58b
ahmednashaat2 <ahmednashaat96@hotmail.com>
2017-10-25 14:21:28 +0200

    The test case of file path pic is automated

--- src/com/chess/engine/ChessMain.java
+++ src/com/chess/engine/ChessMain.java
@@ -3,11 +3,21 @@ package com.chess.engine;
 import com.chess.engine.board.Board;
 import com.chess.gui.Table;
 
+import java.io.File;
+
 public class ChessMain {
     public static void main(String[] args){
         Board board = Board.createStandardBoard();
         System.out.println(board);
         new Table();
-
+        File file = new File("art/fancy/BB.gif");// write here your file path //
+        if (file.exists()) {
+            System.out.println(file.getPath()+(" exists"));
+        }
+        else
+            System.out.println(file.getPath()+(" Not exists"));
     }
+
+
 }
+
